7857c4dd0e8226a859921d82eb5cc4c25f4999d1
gossoudarev <igossoudarev@herzen.spb.ru>
2017-10-25 12:22:59 +0300

    24102017

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ class JSONReader {
   URL = 'http://kodaktor.ru/j/users';
   async readJSON(){
       const  {data:{users}} = await axios.get(this.URL);
+      document.querySelector('h1').textContent = 'Hey!';
       document.querySelector('.cont').innerHTML=JSON.stringify(users)
   }
 }
 
 const myReader = new JSONReader();
 myReader.readJSON();
+console.log(aa);
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,14 +1,28 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+const WDS_PORT = 1234;
+const myTitle = 'Demo Webpack !!! 25-10-2017';
+
 export default {
   "entry": "./index.js",
   "output": {
-    "publicPath": "/dist",
-    "filename": "bundle.js"
+    "publicPath": "/",
+    "filename": "./dist/bundle.js"
   },
   "devtool": "sourcemap",
   "module": {
-    "rules": [{
+      "rules": [{
+ 	    "test": /\.js$/,
       "loader": "babel-loader"
     }]
   },
-  "devServer": {"port": 1234}
+  "plugins": [new HtmlWebpackPlugin({
+    "title": myTitle,
+    "template": "./my-index.ejs",
+    "h2": myTitle
+  })],
+  "devServer": {
+    "port": WDS_PORT
+  }
+
 };
+// this file: webpack.config.babel.js fork from https://kodaktor.ru/j/min_react_webpack
